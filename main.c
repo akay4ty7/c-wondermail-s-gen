38eea011134341ca781da4de3ad82991ce32cc0b
akay4ty7 <akay4ty7@users.noreply.github.com>
2025-12-18 16:53:42 +0900

    Notes

--- main.c
+++ main.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
 
+// Multi step process
+// Menu systems (States?)
+// Mission Type: numbered input
+// Dungeon: numbered input
+// Floor: unsigned integer (cannot have negative floors)
+// Client: Pokemon Ids?
+// Target (Pokemon): Find someone, steal from someone etc.
+// Target Item: Find someone with item, find  item somewhere on floor
+// Reward Type: Enum of reward type like Item + ??? etc.
+// Reward: Can Only be single item (dependant on reward type)
+// Yes No for Euro version of game as codes are different.
+
 int main(void) {
-  printf("Hello, World!");
-  
+  printf("Wondermail-S Generator\n");
+
   return 0;
 }
+
